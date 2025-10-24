cd1084e31577a00a233a2d74e459dde6d0497fd6
Husky-tek <65168644+Husky-tek@users.noreply.github.com>
2025-10-24 14:42:11 -0600

    lmao

---
+++ WebPortfo/src/App.tsx
@@ -0,0 +1,13 @@
+    import React from 'react';
+    import Button from '@mui/material/Button';
+    import Typography from '@mui/material/Typography';
+
+    function App() {
+      return (
+        <div>
+           <Button variant="contained">Hello world</Button>
+        </div>
+      );
+    }
+
+    export default App;
