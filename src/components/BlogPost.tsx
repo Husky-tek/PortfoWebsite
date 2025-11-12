9f67745230036d7c7ed73eedbd7ffd361aff99ca
Husky-tek <65168644+Husky-tek@users.noreply.github.com>
2025-11-11 20:34:13 -0700

    lmao2

--- src/components/BlogPost.tsx
+++ src/components/BlogPost.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { Box, Typography, Paper, CircularProgress } from '@mui/material';
 import posts from './Blog/blogposts.json';
